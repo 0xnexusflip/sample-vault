8cc203717ea3f2b9393917906d5beca1b7aa31a7
0xnexusflip <0xnexusflip@gmail.com>
2024-01-11 02:33:21 +0000

    add contract, tests, NatSpec and comments

---
+++ src/Vault.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title ERC20 Vault
+/// @author nexusflip
+/// @notice A simple pausable Vault integration with an ERC20 token whitelist and access control
+contract Vault is Ownable {
+    /// @notice ERC20 addresses for which token is whitelisted (true -» whitelisted, false -» !whitelisted)
+    mapping(address => bool) private whitelistedTokens;
+
+    /// @dev User address -» ERC20 address -» Amount deposited
+    mapping(address => mapping(address => uint256)) public deposits;
+
+    /// @notice Flag to pause contract if owner
+    bool public paused; 
+
+    /// @notice Emitted when deposit is executed
+    /// @param user Address of depositor
+    /// @param token ERC20 address for whitelisted token deposited by user
+    /// @param amount Amount deposited
+    event Deposit(address indexed user, address indexed token, uint256 amount);
+
+    /// @notice Emitted when withdraw is executed
+    /// @param user Address of withdrawer
+    /// @param token ERC20 address for whitelisted token deposited by user
+    /// @param amount Amount withdrawn
+    event Withdraw(address indexed user, address indexed token, uint256 amount);
+
+    /// @notice Checks if contract is paused
+    /// @dev If paused, reverts
+    modifier whenNotPaused() {
+        require(!paused, "Vault is paused");
+        _;
+    }
+
+    /// @notice Checks if ERC20 token address is whitelisted
+    /// @param _token ERC20 token address to check
+    /// @dev If not whitelisted, reverts
+    modifier onlyWhitelisted(address _token) {
+        require(whitelistedTokens[_token], "Token not whitelisted");
+        _;
+    }
+
+    /// @notice Contract constructor (inherits Ownable properties for access control)
+    /// @notice Contract deployer address gets Admin access
+    /// @dev unpaused by default
+    constructor() Ownable(msg.sender) {
+        paused = false;
+    }
+
+    /// @notice Pauses contract (disables usage)
+    /// @notice Can only be called by Admin
+    function pause() public onlyOwner {
+        paused = true;
+    }
+
+    /// @notice Unpauses contract (resumes usage)
+    /// @notice Can only be called by Admin
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    /// @notice Whitelists ERC20 address, enabling deposits
+    /// @notice Can only be called by Admin
+    function whitelistToken(address _token) public onlyOwner {
+        whitelistedTokens[_token] = true;
+    }
+
+    /// @notice Address that calls function deposits given amount of respective ERC20 token
+    /// @param _token ERC20 address of token to deposit
+    /// @param _amount Amount of _token to deposit
+    /// @dev Emits Deposit event
+    /// @dev Requires contract to be unpaused and ERC20 token address to be whitelisted
+    /// @dev If user balance lt amount of token to deposit, reverts
+    function deposit(address _token, uint256 _amount) public whenNotPaused onlyWhitelisted(_token) {
+        require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+        deposits[msg.sender][_token] += _amount;
+
+        emit Deposit(msg.sender, _token, _amount);
+    }
+
+    /// @notice Address that calls function withdraws given amount of respective ERC20 token
+    /// @param _token ERC20 address of token to withdraw
+    /// @param _amount Amount of _token to withdraw
+    /// @dev Emits Withdraw event
+    /// @dev Requires contract to be unpaused and ERC20 token address to be whitelisted
+    /// @dev If amount of token deposited lt amount of token withdraw, reverts
+    function withdraw(address _token, uint256 _amount) public whenNotPaused onlyWhitelisted(_token) {
+        require(deposits[msg.sender][_token] >= _amount, "Insufficient balance");
+        deposits[msg.sender][_token] -= _amount;
+
+        require(IERC20(_token).transfer(msg.sender, _amount), "Transfer failed");
+
+        emit Withdraw(msg.sender, _token, _amount);
+    }
+}
---
+++ test/Vault.t.sol
@@ -0,0 +1,245 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+import "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract VaultTest is Test {
+    /// PUBLIC VARIABLES
+    Vault public vault;
+    ERC20Mock public token;
+
+    uint256 public depositAmount;
+    uint256 public mintAmount;
+
+    // EVENTS
+
+    event Deposit(address indexed user, address indexed token, uint256 amount);
+    event Withdraw(address indexed user, address indexed token, uint256 amount);
+
+    /// TESTING SUITE SET-UP
+
+    function setUp() public {
+        //Init vault and mock ERC20
+        vault = new Vault();
+        token = new ERC20Mock();
+
+        //Amount to mint for test
+        mintAmount = 1000 ether;
+
+        // Mint tokens
+        token.mint(address(this), mintAmount);
+
+        // Approve tokens for Vault usage
+        token.approve(address(vault), type(uint256).max);
+        
+        // Mock amount for testing
+        depositAmount = 100 ether;
+    }
+
+    /// END-TO-END TESTING
+
+    /// @notice End-to-end test for user deposit and withdraw
+    function testDepositWithdraw() public {
+        //Whitelist ERC20 token
+        vault.whitelistToken(address(token));
+
+        //Deposit token
+        vault.deposit(address(token), depositAmount);
+
+        //Assert deposit amount
+        assertEq(token.balanceOf(address(vault)), depositAmount);
+        assertEq(vault.deposits(address(this), address(token)), depositAmount);
+
+        //Withdraw token
+        vault.withdraw(address(token), depositAmount);
+
+        //Assert withdrawn amount
+        assertEq(token.balanceOf(address(this)), mintAmount);
+    }
+
+    /// @notice End-to-end test for pausing and unpausing contract
+    function testPauseAndUnpause() public {
+        //Whitelist ERC20 token
+        vault.whitelistToken(address(token));
+        
+        //Pause contract
+        vault.pause();
+
+        //Expect revert
+        try vault.deposit(address(token), depositAmount) {
+            fail("Deposit should fail when paused");
+        } catch {}
+
+        //Unpause contract
+        vault.unpause();
+        
+        //Expect pass
+        vault.deposit(address(token), depositAmount);
+    }
+
+    /// @notice End-to-end test for multiple user deposit and withdraws
+    function testMultipleUsersDepositWithdraw() public {
+        //Setup additional user accounts and ERC20 mock token
+        address user2 = address(0x2);
+        vm.deal(user2, 1 ether);
+        ERC20Mock token2 = new ERC20Mock();
+        
+        //Whitelist ERC20 tokens
+        vault.whitelistToken(address(token));
+        vault.whitelistToken(address(token2));
+
+        token2.mint(user2, 500 ether);
+        vm.prank(user2);
+        token2.approve(address(vault), type(uint256).max);
+
+        //Alice deposit
+        uint256 aliceDeposit = 100 ether;
+        vault.deposit(address(token), aliceDeposit);
+
+        //Bob deposit
+        vm.prank(user2);
+        uint256 bobDeposit = 200 ether;
+        vault.deposit(address(token2), bobDeposit);
+
+        //Assert balances for Alice and Bob
+        assertEq(vault.deposits(address(this), address(token)), aliceDeposit);
+        assertEq(vault.deposits(user2, address(token2)), bobDeposit);
+
+        //Alice and Bob withdraw
+        vault.withdraw(address(token), aliceDeposit);
+        vm.prank(user2);
+        vault.withdraw(address(token2), bobDeposit);
+
+        //Assert balances for Alice and Bob
+        assertEq(token.balanceOf(address(this)), 1000 ether);
+        assertEq(token2.balanceOf(user2), 500 ether);
+    }
+
+    /// FUZZING
+
+    ///@notice Fuzz test for checking revert when withdraw amount gt user deposit
+    function testFuzzWithdrawMoreThanDeposit(uint256 amount) public {
+        //Whitelist ERC20 token
+        vault.whitelistToken(address(token));
+        
+        //Deposit token
+        vault.deposit(address(token), amount);
+
+        //Expect revert
+        if (amount > 100 ether) {
+            try vault.withdraw(address(token), amount) {
+                fail("Should not be able to withdraw more than deposited");
+            } catch {}
+        }
+    }
+
+    /// EVENT TESTING
+
+    /// @notice Unit test for checking deposit event
+    function testDepositEmit() public {
+        //Whitelist ERC20
+        vault.whitelistToken(address(token));
+
+        //Expect event
+        vm.expectEmit(true, true, false, true);
+        emit Deposit(address(this), address(token), depositAmount);
+
+        //Withdraw amount
+        vault.deposit(address(token), depositAmount);
+    }
+
+    /// @notice Unit test for checking withdraw event
+    function testWithdrawEmit() public {
+        //Whitelist ERC20
+        vault.whitelistToken(address(token));
+
+        //Deposit amount
+        vault.deposit(address(token), depositAmount);
+
+        //Expect event
+        vm.expectEmit(true, true, false, true);
+        emit Withdraw(address(this), address(token), depositAmount);
+
+        //Withdraw amount
+        vault.withdraw(address(token), depositAmount);
+    }
+
+    /// REVERT TESTING
+
+    ///@notice Revert test for pausing contract without access control
+    function testFailUnauthorizedPause() public {
+        //Set address to non-authorized
+        vm.prank(address(0x123));
+
+        //Expect access control revert when attempting pause
+        vault.pause();
+    }
+
+    ///@notice Revert test for unpausing contract without access control
+    function testFailUnauthorizedUnpause() public {
+        //Set address to non-authorized
+        vm.prank(address(0x123));
+
+        //Expect access control revert when attempting unpause
+        vault.unpause();
+    }
+
+    ///@notice Revert test for whitelisting ERC20 token address without access control
+    function testFailUnauthorizedWhitelistToken() public {
+        //Set address to non-authorized
+        vm.prank(address(0x123));
+
+        //Expect access control revert when attempting token whitelisting
+        vault.whitelistToken(address(0x456));
+    }
+
+    /// @notice Revert test for depositing when Vault is paused
+    function testFailDepositWhenPaused() public {
+        //Whitelist ERC20 token
+        vault.whitelistToken(address(token));
+
+        //Pause contract
+        vault.pause();
+        
+        //Attempt deposit
+        vault.deposit(address(token), 50 ether);
+
+        //Expect pause revert
+        vm.expectRevert("Vault is paused");
+    }
+
+    /// @notice Revert test for withdrawing when Vault is paused
+    function testFailWithdrawWhenPaused() public {
+        //Whitelist ERC20 token
+        vault.whitelistToken(address(token));
+
+        //Deposit amount
+        vault.deposit(address(token), 50 ether);
+
+        //Pause contract
+        vault.pause();
+        
+        //Attempt withdraw
+        vault.withdraw(address(token), 50 ether);
+
+        //Expect pause revert
+        vm.expectRevert("Vault is paused");
+    }
+
+    /// @notice Revert test for checking deposit with non-whitelisted token
+    function testFailDepositWithNonWhitelistedToken() public {
+        //Init non-whitelisted ERC20 token
+        ERC20Mock nonWhitelistedToken = new ERC20Mock();
+
+        //Mint token
+        nonWhitelistedToken.mint(address(this), depositAmount);
+
+        //Deposit non-whitelisted token amount
+        vault.deposit(address(nonWhitelistedToken), depositAmount);
+
+        //Expect revert
+        vm.expectRevert();
+    }
+}
