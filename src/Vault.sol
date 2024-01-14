1eb0d1d214504aa1caddaad2508bcef6bf00c1ab
0xnexusflip <0xnexusflip@gmail.com>
2024-01-14 01:30:23 +0000

    add safeTransaction support

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @title ERC20 Sample Vault
 /// @notice A simple pausable Vault integration with an ERC20 token whitelist and access control
 /// @dev Inherits OpenZeppelins' Ownable contract
 contract Vault is Ownable {
+    /// Add support for safeTransfer wrapper
+    using SafeERC20 for IERC20;
+
     /// @notice ERC20 addresses for which token is whitelisted (true -» whitelisted, false -» !whitelisted)
     mapping(address => bool) private whitelistedTokens;
 
@@ -91,10 +94,8 @@ contract Vault is Ownable {
         uint256 _amount
     ) public whenNotPaused onlyWhitelisted(_token) notZeroAddress(msg.sender) {
         require(_amount > 0, "Cannot deposit 0");
-        require(
-            IERC20(_token).transferFrom(msg.sender, address(this), _amount),
-            "Transfer failed"
-        );
+
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
 
         deposits[msg.sender][_token] += _amount;
 
@@ -120,11 +121,8 @@ contract Vault is Ownable {
             "Insufficient balance"
         );
         deposits[msg.sender][_token] -= _amount;
-
-        require(
-            IERC20(_token).transfer(msg.sender, _amount),
-            "Transfer failed"
-        );
+        
+        IERC20(_token).safeTransfer(msg.sender, _amount);
 
         emit Withdraw(msg.sender, _token, _amount);
     }
